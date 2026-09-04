4ba8167b38c4ba5638f9bae53be3beaefe3f3d20
Marco Scardovi <scardracs@disroot.org>
2026-09-04 22:34:06 +0200

    leds: Add LED_DYNAMIC_LIGHTING flag to LED core
    Define LED_DYNAMIC_LIGHTING flag in struct led_classdev to enable
    runtime type identification for Dynamic Lighting class devices,
    matching the established pattern used by LED_MULTI_COLOR.
    
    Signed-off-by: Marco Scardovi <scardracs@disroot.org>

--- include/linux/leds.h
+++ include/linux/leds.h
@@ -109,6 +109,7 @@ struct led_classdev {
 #define LED_INIT_DEFAULT_TRIGGER BIT(23)
 #define LED_REJECT_NAME_CONFLICT BIT(24)
 #define LED_MULTI_COLOR		BIT(25)
+#define LED_DYNAMIC_LIGHTING	BIT(26)
 
 	/* set_brightness_work / blink_timer flags, atomic, private. */
 	unsigned long		work_flags;
